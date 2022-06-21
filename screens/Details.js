750e912aaef2d82f005d900cf73e7fa94439b326
ima <selfloveima.g@gmail.com>
2022-06-21 22:31:36 +0100

    first commit

---
+++ screens/Details.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {text} from 'react-native'
+const Details = () => {
+	return (
+		<text>Details</text>
+	)
+}
+
+export default Details
---
+++ screens/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {useState} from 'react';
+import {view, SafeArea, FlatList, Text} from 'react-native';
+import {COLORS, NFTData} from '../constants';
+const Home = () => {
+	return (
+	  <SafeAreaView style={{flex: 1}}>
+         
+	  </SafeAreaView>
+	)
+}
+
+export default Home
